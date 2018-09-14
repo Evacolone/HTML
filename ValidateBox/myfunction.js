0d8b59eede22aea4af497ee0b12bf7cf56c75ce3
Evacolone <2359713771@qq.com>
2018-09-14 22:52:44 +0800

    914
    excise

---
+++ ValidateBox/myfunction.js
@@ -0,0 +1,6 @@
+function myfunction(){
+    var x = document.getElementById("demo").Value;
+    if(x == ""||isNaN(x)){
+        alert("不是数字")
+    }
+}
--- readme.md
+++ readme.md
@@ -1,1 +1,2 @@
 ## this is HTML file
+## excise
